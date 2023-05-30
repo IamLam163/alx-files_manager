54def5a8166fd67f3f949d7e9feac45926ddea1c
IamLam163 <olamidealiyu@gmail.com>
2023-05-31 00:16:19 +0100

    all changes

--- main1.js
+++ main1.js
@@ -1,5 +1,5 @@
 import dbClient from './utils/db';
-
+/*
 const waitConnection = () => {
   return new Promise((resolve, reject), => {
   let i = 0;
@@ -21,3 +21,34 @@ const waitConnection = () => {
 })
 };
 
+*/
+const waitConnection = () => {
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    const repeatFct = async () => {
+      await setTimeout(() => {
+        i += 1;
+        if (i >= 10) {
+          reject(new Error('Connection timeout'));
+        } else if (!dbClient.isAlive()) {
+          repeatFct();
+        } else {
+          resolve();
+        }
+      }, 1000);
+    };
+    repeatFct();
+  });
+};
+
+(async () => {
+  console.log(dbClient.isAlive());
+  try {
+    await waitConnection();
+    console.log(dbClient.isAlive());
+    console.log(await dbClient.nbUsers());
+    console.log(await dbClient.nbFiles());
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+})();
