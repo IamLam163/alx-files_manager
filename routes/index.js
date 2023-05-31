7054b0a766c111a9e291cf83971b924954aa7956
IamLam163 <olamidealiyu@gmail.com>
2023-05-31 15:39:28 +0100

    user controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import APPController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', APPController.getStatus);
 router.get('/stats', APPController.getStats);
-router.post('/users');
+router.post('/users', UsersController);
 
 export default router;
